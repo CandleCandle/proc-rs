a8523f1472e86fde273d2a6a09a4618318ce303c
Candle <candle@candle.me.uk>
2025-10-27 18:45:22 +0000

    [feature] add parsing of processes

--- src/data/rl_data_parse.rs
+++ src/data/rl_data_parse.rs
@@ -1,8 +1,8 @@
-use std::{collections::{BTreeMap, HashMap}, default, rc::Rc};
+use std::{collections::{BTreeMap, HashMap}, rc::Rc};
 
 use serde_json::Value;
 
-use crate::data::{dataset::DataSetConf, model::Classification};
+use crate::data::{dataset::DataSetConf, model::{Classification, Stack}};
 
 use super::model::{Data, DataParser, Factory, FactoryGroup, Item, Process};
 
@@ -16,6 +16,7 @@ pub enum DataParserRecipeListerFiles {
     MiningDrill,
     Items,
     Fluids,
+    Recipe,
 }
 impl DataParserRecipeListerFiles {
     pub fn to_key(&self) -> String {
@@ -26,6 +27,7 @@ impl DataParserRecipeListerFiles {
             Self::MiningDrill => "MiningDrill".to_string(),
             Self::Items => "Items".to_string(),
             Self::Fluids => "Fluids".to_string(),
+            Self::Recipe => "Recipe".to_string(),
         }
     }
 }
@@ -102,7 +104,7 @@ impl DataParserRecipeLister {
                             Rc::new(
                                 Factory{
                                     id: obj.get("name").ok_or(format!("missing name in {k:?} at {id}"))?
-                                        .as_str().ok_or(format!("missing name in {k:?} from {id} was not a string"))?
+                                        .as_str().ok_or(format!("name in {k:?} from {id} was not a string"))?
                                         .to_string(),
                                     display: obj["name"].as_str().unwrap().to_string(), // TOOD import i18n
                                     duration_multiplier: 1.0/obj[duration_key].as_f64().unwrap(),
@@ -114,7 +116,7 @@ impl DataParserRecipeLister {
                                         .map(
                                             |cc_key| -> Result<Rc<FactoryGroup>, String> {
                                                 Ok(factory_groups.get(&cc_key)
-                                                    .ok_or(format!("unable to find a factor group {cc_key} in {k:?} at {id}"))?
+                                                    .ok_or(format!("unable to find a factory group {cc_key} in {k:?} at {id}"))?
                                                     .clone())
                                         })
                                         .collect::<Result<Vec<Rc<FactoryGroup>>, String>>()?
@@ -163,6 +165,105 @@ impl DataParserRecipeLister {
 
         Ok(items)
     }
+
+    fn extract_processes(
+            parsed: &BTreeMap<String, Value>,
+            factory_groups: &HashMap<String, Rc<FactoryGroup>>,
+            items: &HashMap<String, Rc<Item>>
+        ) -> Result<HashMap<String, Rc<Process>>, String> {
+        let mut processes: HashMap<String, Rc<Process>> = HashMap::new();
+
+        for k in &[
+            DataParserRecipeListerFiles::Recipe,
+        ] {
+            processes.extend(
+                parsed.get(&k.to_key()).ok_or(format!("missing json {k:?}"))?
+                    .as_object().ok_or(format!("missing root object in {k:?}"))?
+                    .into_iter()
+                    .map(|(id, val)| -> Result<(String, Rc<Process>), String> {
+                        let map = val.as_object().ok_or(format!("item at {id} in {k:?} was not an object"))?;
+                        Ok((id.clone(), Rc::new(
+                            Process{
+                                id: id.clone(),
+                                display: map.get("name").unwrap().as_str().unwrap().to_string(),
+                                duration: map.get("energy").ok_or(format!("missing [{id}.\"energy\"] in {k:?}"))?.as_f64().unwrap(),
+                                group: factory_groups.get(map.get("category").unwrap().as_str().unwrap()).unwrap().clone(),
+                                inputs: Self::extract_mod(
+                                    map.get("ingredients").ok_or(format!("missing [{id}].\"ingredients\" from {k:?}"))?,
+                                    items, &id, &k)?,
+                                outputs: Self::extract_mod(
+                                    map.get("products").ok_or(format!("missing [{id}].\"products\" from {k:?}"))?,
+                                    items, &id, &k)?,
+                                inputs_unmod: Self::extract_unmod(
+                                    map.get("ingredients").ok_or(format!("missing [{id}].\"products\" from {k:?}"))?,
+                                    items, &id, &k)?,
+                                outputs_unmod: Self::extract_unmod(
+                                    map.get("products").ok_or(format!("missing [{id}].\"products\" from {k:?}"))?,
+                                    items, &id, &k)?,
+                            }
+                        )))
+                    })
+                    .collect::<Result<Vec<(String, Rc<Process>)>, String>>()?
+            )
+        }
+        Ok(processes)
+    }
+
+    fn extract_unmod(stacks: &Value, items: &HashMap<String, Rc<Item>>, id: &String, k: &DataParserRecipeListerFiles) -> Result<Vec<Stack>, String> {
+        if stacks.as_object().is_some() {
+            return Ok(Vec::new())
+        }
+
+        stacks.as_array().ok_or(format!("[{id}].\"ingredients|products\" from {k:?} was not an array or empty object"))?
+            .iter().enumerate()
+            .map(|(idx, stack)| {
+                // XXX work out how "extra_count_fraction" impacts this calculation.
+                let obj = stack.as_object().ok_or(format!("[{id}].\"ingredients|products\"[{idx}] from {k:?} was not an object"))?;
+                tracing::info!("{:?}", obj);
+                let item = items.get(
+                    obj.get("name").ok_or("todo!1")?
+                        .as_str().ok_or("todo!2")?
+                    ).ok_or(format!("unable to find item at [{id}].\"ingredients|products\"[{idx}] from {k:?}"))?
+                    .clone();
+                let quantity = obj.get("amount").ok_or("todo!4")?.as_f64().ok_or("todo!5")?;
+                let ignored = obj.get("ignored_by_productivity")
+                    .map(|v| -> Result<f64, &str> { v.as_f64().ok_or("todo!6") })
+                    .unwrap_or(Ok(0.0))?;
+                Ok(Stack{
+                    item,
+                    quantity: ignored.min(quantity),
+                })
+            })
+            .collect::<Result<Vec<Stack>, String>>()
+    }
+
+    fn extract_mod(stacks: &Value, items: &HashMap<String, Rc<Item>>, id: &String, k: &DataParserRecipeListerFiles) -> Result<Vec<Stack>, String> {
+        if stacks.as_object().is_some() {
+            return Ok(Vec::new())
+        }
+
+        stacks.as_array().ok_or(format!("[{id}].\"ingredients|products\" from {k:?} was not an array or empty object"))?
+            .iter().enumerate()
+            .map(|(idx, stack)| {
+                // XXX work out how "extra_count_fraction" impacts this calculation.
+                let obj = stack.as_object().ok_or(format!("[{id}].\"ingredients|products\"[{idx}] from {k:?} was not an object"))?;
+                tracing::info!("{:?}", obj);
+                let item = items.get(
+                    obj.get("name").ok_or("todo!1")?
+                        .as_str().ok_or("todo!2")?
+                    ).ok_or(format!("unable to find item at [{id}].\"ingredients|products\"[{idx}] from {k:?}"))?
+                    .clone();
+                let quantity = obj.get("amount").ok_or("todo!4")?.as_f64().ok_or("todo!5")?;
+                let ignored = obj.get("ignored_by_productivity")
+                    .map(|v| -> Result<f64, &str> { v.as_f64().ok_or("todo!6") })
+                    .unwrap_or(Ok(0.0))?;
+                Ok(Stack{
+                    item,
+                    quantity: (quantity-ignored).max(0.0),
+                })
+            })
+            .collect::<Result<Vec<Stack>, String>>()
+    }
 }
 
 
@@ -184,32 +285,11 @@ impl DataParser for DataParserRecipeLister {
         let factory_groups = Self::extract_factory_groups(&parsed)?;
         let factories = Self::extract_factories(&parsed, &factory_groups)?;
         let items = Self::extract_items(&parsed)?;
-        let processes: HashMap<String, Rc<Process>> = HashMap::new();
-
-        // processes
-        // recipe.json
-        //
-        // resource.json --> producers of raw resources inputs can be added
-
-        // factories (and groups)
-        // assembling-machine.json
-        // furnace.json
-        // rocket-silo.json
-        //
-        // mining-drill.json resource_categories -> factory-group; resource_category -> factory-group
-        //
-        //
-        // lab.json + fake output?
-        //
-        // "items"
-        // fluid.json
-        // item.json
+        let processes = Self::extract_processes(&parsed, &factory_groups, &items)?;
 
         Ok(Data{items, factory_groups, factories, processes})
     }
 }
-
-
 
 #[cfg(test)]
 mod test {
@@ -220,18 +300,22 @@ mod test {
     use crate::data::{fixtures::load_fixture};
 
     use super::*;
+    static INIT: std::sync::Once = std::sync::Once::new();
 
     fn setup_tracing() {
-        tracing_subscriber::registry()
-            .with(tracing_subscriber::fmt::Layer::default()
-            .with_ansi(true)
-            .with_writer(std::io::stdout)
-            .compact())
-            .try_init().map_err(|e| e.to_string())
-            .unwrap();
+        INIT.call_once(|| {
+            tracing_subscriber::registry()
+                .with(tracing_subscriber::fmt::Layer::default()
+                .with_ansi(true)
+                .with_writer(std::io::stdout)
+                .compact())
+                .try_init().map_err(|e| e.to_string())
+                .unwrap();
+        });
     }
 
     fn create_input_fixture() -> BTreeMap<String, String> {
+        setup_tracing();
         let mut jsons = BTreeMap::new();
         jsons.insert(DataParserRecipeListerFiles::AssemblingMachines.to_key().to_string(), load_fixture("fixtures/assembling-machine.json").to_string());
         jsons.insert(DataParserRecipeListerFiles::Furnace.to_key().to_string(), load_fixture("fixtures/furnace.json").to_string());
@@ -239,11 +323,13 @@ mod test {
         jsons.insert(DataParserRecipeListerFiles::MiningDrill.to_key().to_string(), load_fixture("fixtures/mining-drill.json").to_string());
         jsons.insert(DataParserRecipeListerFiles::Fluids.to_key().to_string(), load_fixture("fixtures/fluid.json").to_string());
         jsons.insert(DataParserRecipeListerFiles::Items.to_key().to_string(), load_fixture("fixtures/item.json").to_string());
+        jsons.insert(DataParserRecipeListerFiles::Recipe.to_key().to_string(), load_fixture("fixtures/recipe.json").to_string());
         jsons
     }
 
     #[test]
     fn it_loads_all_factory_groups() {
+        setup_tracing();
         let mut jsons = create_input_fixture();
         let res = DataParserRecipeLister{}.parse(&mut jsons);
         let r = res.unwrap();
@@ -269,6 +355,7 @@ mod test {
 
     #[test]
     fn it_loads_all_factories() {
+        setup_tracing();
         let mut jsons = create_input_fixture();
         let res = DataParserRecipeLister{}.parse(&mut jsons);
         let r = res.unwrap();
@@ -288,6 +375,7 @@ mod test {
 
     #[test]
     fn it_loads_info_for_factories() {
+        setup_tracing();
         let mut jsons = create_input_fixture();
         let res = DataParserRecipeLister{}.parse(&mut jsons);
         let r = res.unwrap();
@@ -310,6 +398,7 @@ mod test {
 
     #[test]
     fn it_loads_info_for_mining_drills_into_factories() {
+        setup_tracing();
         let mut jsons = create_input_fixture();
         let res = DataParserRecipeLister{}.parse(&mut jsons);
         let r = res.unwrap();
@@ -328,15 +417,59 @@ mod test {
 
     #[test]
     fn it_loads_all_items() {
+        setup_tracing();
         let mut jsons = create_input_fixture();
         let res = DataParserRecipeLister{}.parse(&mut jsons);
         let r = res.unwrap();
         assert_eq!(r.items.values().map(|fg| fg.id.clone()).sorted().collect::<Vec<String>>(), &[
+            "advanced-circuit",
+            "big-mining-drill",
+            "electric-engine-unit",
+            "electric-mining-drill",
             "iron-chest",
             "steam",
             "steel-chest",
+            "tungsten-carbide",
+            "uranium-235",
+            "uranium-238",
             "water",
+            "wood",
             "wooden-chest",
         ])
+    }
+
+    #[test]
+    fn it_loads_all_processes() {
+        setup_tracing();
+        let mut jsons = create_input_fixture();
+        let res = DataParserRecipeLister{}.parse(&mut jsons);
+        let r = res.unwrap();
+        assert_eq!(r.processes.values().map(|fg| fg.id.clone()).sorted().collect::<Vec<String>>(), &[
+            "big-mining-drill-recycling",
+            "kovarex-enrichment-process",
+            "parameter-0",
+            "wooden-chest",
+        ])
+    }
+
+    #[test]
+    fn it_understands_process_with_unmodifiable_io() {
+        setup_tracing();
+        let mut jsons = create_input_fixture();
+        let res = DataParserRecipeLister{}.parse(&mut jsons);
+        let r = res.unwrap();
+        let process = r.processes.get("kovarex-enrichment-process").unwrap();
+        assert_eq!(process.id, "kovarex-enrichment-process");
+        assert_eq!(process.display, "kovarex-enrichment-process");
+        assert_eq!(process.duration, 60.0);
+        assert_eq!(process.group.id, "centrifuging");
+        assert_eq!(process.inputs.iter().map(|s| s.item.id.clone()).sorted().collect::<Vec<String>>(), &["uranium-235", "uranium-238"]);
+        assert_eq!(process.inputs.iter().sorted_by(|a, b| a.item.id.cmp(&b.item.id)).map(|s| s.quantity).collect::<Vec<f64>>(), &[40.0, 5.0]);
+        assert_eq!(process.outputs.iter().map(|s| s.item.id.clone()).sorted().collect::<Vec<String>>(), &["uranium-235", "uranium-238"]);
+        assert_eq!(process.outputs.iter().sorted_by(|a, b| a.item.id.cmp(&b.item.id)).map(|s| s.quantity).collect::<Vec<f64>>(), &[1.0, 0.0]);
+        assert_eq!(process.inputs_unmod.iter().map(|s| s.item.id.clone()).sorted().collect::<Vec<String>>(), &["uranium-235", "uranium-238"]);
+        assert_eq!(process.inputs_unmod.iter().sorted_by(|a, b| a.item.id.cmp(&b.item.id)).map(|s| s.quantity).collect::<Vec<f64>>(), &[0.0, 0.0]);
+        assert_eq!(process.outputs_unmod.iter().map(|s| s.item.id.clone()).sorted().collect::<Vec<String>>(), &["uranium-235", "uranium-238"]);
+        assert_eq!(process.outputs_unmod.iter().sorted_by(|a, b| a.item.id.cmp(&b.item.id)).map(|s| s.quantity).collect::<Vec<f64>>(), &[40.0, 2.0]);
     }
 }
